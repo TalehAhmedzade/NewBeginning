df0abd4f42f8503dfaaec5cac4ec43d6f56113b0
Taleh <taleh.ahmed2004@gmail.com>
2024-02-08 13:42:56 +0400

    Add project files.

---
+++ NewBeginning/Program.cs
@@ -0,0 +1,19 @@
+﻿using NewBeginning.Entities;
+
+Console.WriteLine("Sa");
+string massage1 = "Life is hard, but you are strong, my brother. JUST DO IT";
+int value = 111;
+//   start      condition  increment
+for(int i = 0; i < value; i++)
+{
+	Console.WriteLine(massage1);
+}
+string[] loans = { "Kredit1", "2", "3" };
+for(int i = 0;i < loans.Length; i++)
+	Console.WriteLine(loans[i]);
+string[] loans2 = new string[]
+{
+
+};
+//create referance 
+Course course1 = new Course();
