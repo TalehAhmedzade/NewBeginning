6fa74b0ab0ef0a2c3c895d1a505ae13ac03b209c
Taleh <taleh.ahmed2004@gmail.com>
2024-02-13 18:56:15 +0400

    LEsson 2 episode 1

---
+++ NewBeginning/DataAcess/Concretes/CourseDal.cs
@@ -0,0 +1,16 @@
+﻿using NewBeginning.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewBeginning.DataAcess.Concretes;
+
+public class CourseDal //Data Acess VB ile elaqe
+{
+	public List<Course> GetAll()
+	{
+		//Data BAse Isleri yerine yetirilir . SQL oyren
+	}
+}
--- NewBeginning/Program.cs
+++ NewBeginning/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NewBeginning.Business;
 using NewBeginning.Entities;
+using System;
 
 Console.WriteLine("Sa");
 string massage1 = "Life is hard, but you are strong, my brother. JUST DO IT";
@@ -20,3 +21,41 @@ string[] loans2 = new string[]
 Course course1 = new Course();
 CourseManager manager1 = new();
 manager1.GetAll();
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678922";
+customer1.FirstName = "Aslı";
+customer1.LastName = "Karayiğit";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "56478965412";
+customer2.FirstName = "Özgür";
+customer2.LastName = "Atılgan";
+customer2.CustomerNumber = "123457";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "Kodlamaio";
+customer3.CustomerNumber = "654778";
+customer3.TaxNumber = "12345678987";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "Abc";
+customer4.CustomerNumber = "654779";
+customer4.TaxNumber = "12345678985";
+
+// value types-- > int, bool,double...
+//reference types --> array, class, interface...
+                              //101        //102     //103      //104
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+//polymorphism
+foreach (BaseCustomer customer in customers)
+{
+
+	Console.WriteLine(customer.CustomerNumber);
+}
